360bc991b14d35626492f6be9d446696ebfa9e00
Dror Speiser <dror.mastershin@gmail.com>
2019-05-15 00:46:53 +0200

    Update MPDEF_DIRECTIVE.hpp

--- MPDEF_DIRECTIVE.hpp
+++ MPDEF_DIRECTIVE.hpp
@@ -1,7 +1,7 @@
 //
 // Copyright Jason Rice 2015-2016
 // Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at
+// (See accompanying file LICENSE_boost or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
 #ifndef NBDL_MPDEF_DIRECTIVE_HPP
